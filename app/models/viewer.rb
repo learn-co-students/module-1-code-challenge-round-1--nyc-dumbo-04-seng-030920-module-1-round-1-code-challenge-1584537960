1b18ed31e7682474e81737ce40397a5e0b097a41
Xavier Carty <xaviercarty@gmail.com>
2020-03-18 10:52:51 -0400

    my code

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,33 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def reviews 
+    Review.all.select do |review| 
+      review.viewer == self  
+    end 
+  end
+  def reviewer 
+    reviews.map {|review| review.viewer}.uniq
+  end
+
+  def reviewed_movie?(movie)
+    reviewed = reviews.map {|viewed| viewed.movie == movie }
+     
+    reviewed.map do |reviews| 
+      if reviews 
+        return true 
+      else 
+       return false 
+      end 
+    end
+       
+  end 
+  # didnt finish
+   def rate_movie(movie , rating)
+      if self.reviewed_movie?(movie)
+         
+
+
+   end 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -10,8 +10,18 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+xavier = Viewer.new("xaviercarty")
+donny = Viewer.new("donny")
+
+movie1 = Movie.new("Honey i shrunk the kids")
+movie2 = Movie.new("Above the rims")
+movie3 = Movie.new("Juice")
 
 
+review1 = Review.new(xavier , movie1 , 7)
+review1 = Review.new(xavier , movie2 , 9)
+review2 = Review.new(donny , movie1 , 10)
+review3 = Review.new(donny , movie2 , 20)
 
 
 # ===== WARNING! DO NOT EDIT BELOW THIS LINE ===== #
