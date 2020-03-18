8f3a0d099f267f244f0d0d77584d915923db1d76
Jackmt9 <jackmt9@gmail.com>
2020-03-18 10:29:21 -0400

    Solutions

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie
-  attr_accessor :title
 
+  attr_accessor :title
   @@all = []
 
   def initialize(title)
@@ -10,6 +10,23 @@ class Movie
 
   def self.all
     @@all
+  end
+
+  def reviews
+    Review.all.select{|review| review.movie == self}
+  end
+
+  def reviewers
+    reviews.map(&:viewer)
+  end
+
+  def average_rating
+    reviews.map(&:rating).sum / reviews.length
+  end
+
+  def self.highest_rated
+    sorted = Review.all.sort_by{|review| review.rating}
+    sorted.first.movie
   end
 
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,6 +1,6 @@
 class Viewer
-  attr_accessor :username
 
+  attr_accessor :username
   @@all = []
 
   def initialize(username)
@@ -12,4 +12,26 @@ class Viewer
     @@all
   end
   
+  def reviews
+    Review.all.select{|review| review.viewer == self}
+  end
+
+  def reviewed_movies
+    reviews.map(&:movie)
+  end
+
+  def reviewed_movie?(movie)
+    reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating_num)
+    if reviewed_movie?(movie) 
+      review_of_movie = reviews.select{|review| review.movie == movie}
+      review_of_movie.first.rating = rating_num
+      return review_of_movie
+    else
+      Review.new(self, movie, rating_num)
+    end
+  end
+
 end
