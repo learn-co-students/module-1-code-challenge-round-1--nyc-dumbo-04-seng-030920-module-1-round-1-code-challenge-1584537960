d26e108dc135b3d67b28cb1246d7e18fbd06dbaf
github username <gabriellealexa>
2020-03-18 10:53:10 -0400

    msg

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -1,5 +1,5 @@
 class Viewer
-  attr_accessor :username
+  attr_accessor :username, :review, :rating
 
   @@all = []
 
@@ -11,5 +11,26 @@ class Viewer
   def self.all
     @@all
   end
-  
-end
+
+  def reviews
+    Review.all.select{|viewer|
+      viewer.review == self 
+      }
+  end
+
+  def reviewed_movies
+      Movie.all.select{|viewer|
+        viewer.movie == self
+      }
+    end
+    
+    def reviewed_movie?(movie)
+      Movie.viewer == self
+    end
+
+
+    def rate_movie(movie, rating)
+        self.reviewed_movie?(movie) ? Review.rating = rating : Review.new(self, movie, rating)
+      end 
+
+  end 
